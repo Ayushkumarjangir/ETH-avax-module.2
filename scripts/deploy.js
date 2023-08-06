225e2fee16b7641383e2f301345472633eef7d2a
Nitin-Jangir <137036246+Nitin-Jangir@users.noreply.github.com>
2023-08-06 14:41:20 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,7 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const initBalance = 1;
+  const initBalance = 1; // Replace with your desired initial balance
   const Assessment = await hre.ethers.getContractFactory("Assessment");
   const assessment = await Assessment.deploy(initBalance);
   await assessment.deployed();
