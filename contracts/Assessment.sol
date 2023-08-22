ab2b1394060616a11c39ff050c62cb0c8992b5d6
Ayushkumarjangir <136962198+Ayushkumarjangir@users.noreply.github.com>
2023-08-22 23:45:45 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Assessment {
+contract Ayushkumar {
     address payable public owner;
     mapping(address => uint256) public balanceOf;
 
@@ -13,36 +13,35 @@ contract Assessment {
         balanceOf[msg.sender] = initBalance;
     }
 
-    function getBalance() public view returns(uint256) {
+    function getBalance() public view returns (uint256) {
         return balanceOf[msg.sender];
     }
 
     function deposit() public payable {
-    require(msg.value > 0, "Amount must be greater than 0");
+        require(msg.value > 0, "Amount must be positive value");
 
-    uint256 previousBalance = balanceOf[msg.sender];
-    uint256 newBalance = previousBalance + msg.value;
+        uint256 previousBalance = balanceOf[msg.sender];
+        uint256 newBalance = previousBalance + msg.value;
 
-    // Check for potential overflow
-    require(newBalance >= previousBalance, "Overflow detected");
+        require(newBalance >= previousBalance, "Overflow condition");
 
-    balanceOf[msg.sender] = newBalance;
+        balanceOf[msg.sender] = newBalance;
 
-    // Emit the event
-    emit Deposit(msg.sender, msg.value);
-}
+        emit Deposit(msg.sender, msg.value);
+    }
 
     function withdraw(uint256 _withdrawAmount) public {
-        require(_withdrawAmount > 0, "Amount must be greater than 0");
-        require(balanceOf[msg.sender] >= _withdrawAmount, "Insufficient balance");
+        require(_withdrawAmount > 0, "Amount must be positive");
+        require(
+            balanceOf[msg.sender] >= _withdrawAmount,
+            "Insufficient balance"
+        );
 
         uint256 previousBalance = balanceOf[msg.sender];
         balanceOf[msg.sender] -= _withdrawAmount;
 
-        // Transfer the amount to the sender
         payable(msg.sender).transfer(_withdrawAmount);
 
-        // Emit the event
         emit Withdraw(msg.sender, _withdrawAmount);
         assert(balanceOf[msg.sender] == previousBalance - _withdrawAmount);
     }
